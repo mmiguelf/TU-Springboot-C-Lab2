54c691e739460462e9729f54e8a1d4d73ab1acad
mmiguelf <marwin-m.falqueza@gcash.com>
2025-07-21 22:39:22 +0800

    Added ProductRequestDTO

--- src/main/java/com/example/gcashtrainingspringboot/dto/ProductRequestDTO.java
+++ src/main/java/com/example/gcashtrainingspringboot/dto/ProductRequestDTO.java
@@ -1,4 +1,10 @@
 package com.example.gcashtrainingspringboot.dto;
 
+import lombok.Data;
+
+@Data
 public class ProductRequestDTO {
+    private Long id;
+    private String name;
+    private Double price;
 }
