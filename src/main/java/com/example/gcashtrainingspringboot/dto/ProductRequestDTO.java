e6789e697e3c15598cc004aab9c2673d512ceffb
mmiguelf <marwin-m.falqueza@gcash.com>
2025-07-21 22:49:36 +0800

    Added ProductRequestDTO validation, removed id

--- src/main/java/com/example/gcashtrainingspringboot/dto/ProductRequestDTO.java
+++ src/main/java/com/example/gcashtrainingspringboot/dto/ProductRequestDTO.java
@@ -1,10 +1,13 @@
 package com.example.gcashtrainingspringboot.dto;
 
+import jakarta.validation.constraints.*;
 import lombok.Data;
 
 @Data
 public class ProductRequestDTO {
-    private Long id;
+    @NotBlank(message = "Name must not be a blank")
     private String name;
+
+    @DecimalMin(value = "0.0", inclusive = false, message = "Price must be greater than 0")
     private Double price;
 }
